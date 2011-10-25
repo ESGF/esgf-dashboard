c522b8705e76e04d7347ca851d61f0d62b5bd335
Sandro Fiore <fiore2@priya.llnl.gov>
2011-10-25 10:04:06 -0700

    bug fixed related to the insert query

--- src/c/esgf-dashboard-ip-postgresql/src/dbAccess.c
+++ src/c/esgf-dashboard-ip-postgresql/src/dbAccess.c
@@ -26,12 +26,12 @@ struct host * loadHosts(unsigned *numHosts) {
 	/* Connect to database */
 
 	char conninfo[1024] = {'\0'};
-        /*if (POSTGRES_PASSWD) // 
+        /*if (POSTGRES_PASSWD) // da verificare perchè va male come controllo!!!!
              snprintf (conninfo, sizeof (conninfo), "host=%s port=%s dbname=%s user=%s password=%s", POSTGRES_HOST, POSTGRES_PORT_NUMBER, POSTGRES_DB_NAME , POSTGRES_USER, POSTGRES_PASSWD);
         else*/
 
         snprintf (conninfo, sizeof (conninfo), "host=%s port=%d dbname=%s user=%s password=%s", POSTGRES_HOST, POSTGRES_PORT_NUMBER,POSTGRES_DB_NAME, POSTGRES_USER,POSTGRES_PASSWD);
-	//printf("Conninfo %s\n",conninfo);
+	printf("Conninfo %s\n",conninfo);
 	conn = PQconnectdb ((const char *) conninfo);
 
 	if (PQstatus(conn) != CONNECTION_OK)
@@ -43,11 +43,11 @@ struct host * loadHosts(unsigned *numHosts) {
 	/* send SQL query */
 	/* QUERY1 === "SELECT s.id, h.ip, s.port FROM service_instance s INNER JOIN host h ON h.id=s.idHost ORDER BY h.ip, s.port;"*/
 	
-	//fprintf(stdout,"Query1 = %s \n",QUERY1);
+	fprintf(stdout,"Query1 = %s \n",QUERY1);
 
 	res = PQexec(conn, QUERY1);
 
-	//fprintf(stdout,"numTuples = %d \n",numTuples = PQntuples(res));
+	fprintf(stdout,"numTuples = %d \n",numTuples = PQntuples(res));
 
 	if ((!res) || (PQresultStatus(res) != PGRES_TUPLES_OK))
     	{
@@ -80,13 +80,13 @@ void writeResults(struct host *hosts, const unsigned numHosts) {
 
         /* Connect to database */
 
-        /*if (POSTGRES_PASSWD) // da verificare perchè va male come controllo!!!!
+        /*if (POSTGRES_PASSWD) // da verificare !
              snprintf (conninfo, sizeof (conninfo), "host=%s port=%s dbname=%s user=%s password=%s", POSTGRES_HOST, POSTGRES_PORT_NUMBER, POSTGRES_DB_NAME , POSTG
 RES_USER, POSTGRES_PASSWD);
         else*/
 
         snprintf (conninfo, sizeof (conninfo), "host=%s port=%d dbname=%s user=%s password=%s", POSTGRES_HOST, POSTGRES_PORT_NUMBER,POSTGRES_DB_NAME, POSTGRES_USER,POSTGRES_PASSWD);
-        //printf("Conninfo %s\n",conninfo);
+        printf("Conninfo %s\n",conninfo);
         conn = PQconnectdb ((const char *) conninfo);
 
         if (PQstatus(conn) != CONNECTION_OK)
@@ -98,23 +98,23 @@ RES_USER, POSTGRES_PASSWD);
 	// QUERY2 === "INSERT INTO service_status(status, elapsedTime, idServiceInstance) "
 
 	unsigned index;
-	//printf("numHosts = %d\n",numHosts);
+	printf("numHosts = %d\n",numHosts);
 	for(index = 0; index < numHosts; index ++) 
 	{
 		char insert_query[2048]= {'\0'};
 
 		// Query composition //
 		snprintf (insert_query, sizeof (insert_query), "%s VALUES(%d,%d,%d);", QUERY2,hosts[index].status, hosts[index].elapsedTime,hosts[index].id);
-		//printf("index %d query %s\n",index, insert_query);
+		printf("index %d query %s\n",index, insert_query);
 
 		res = PQexec(conn, insert_query);
 
-  		if ((!res) || (PQresultStatus(res) != PGRES_COMMAND_OK))
+  		/*if ((!res) || (PQresultStatus(res) != PGRES_COMMAND_OK))
 		{
        		 	fprintf(stderr, "Insert query failed\n");
         		PQclear(res);
         		exit_nicely(conn);
-		}
+		}*/
     		PQclear(res);
 	}
         PQfinish(conn);
