fe0bdc524149cf910f7ca8649ca036f8b97534d6
Sandro Fiore <sandro.fiore@unisalento.it>
2013-03-18 22:38:14 +0100

    valgrind scripts added, .gitignore changed, configure.sh for installing the esgf-dashboard module

---
+++ src/c/esgf-dashboard-ip/configure.sh
@@ -0,0 +1,2 @@
+./configure --prefix=/root/Desktop/installLLNL/esgf-dashboard-ip --with-geoip-prefix-path=/root/Desktop/installLLNL/geoIP/ --with-postgres-header-path=/usr/include/ --with-p
+ostgres-lib-path=/usr/lib64/
