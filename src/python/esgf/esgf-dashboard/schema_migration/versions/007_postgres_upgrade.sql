7a10c2d36f6964c608fee22cf9f93ea1d4f87051
Maria Mirto <esgf@esgf-test.cmcc.it>
2016-06-30 09:03:34 -0700

    Add comment at 007_postgres_upgrade.sql

--- src/python/esgf/esgf-dashboard/schema_migration/versions/007_postgres_upgrade.sql
+++ src/python/esgf/esgf-dashboard/schema_migration/versions/007_postgres_upgrade.sql
@@ -294,3 +294,5 @@ INSERT INTO esgf_dashboard.country VALUES ('245', 'ZM', 'Zambia', 'AF');
 INSERT INTO esgf_dashboard.country VALUES ('246', 'ZW', 'Zimbabwe', 'AF');
 INSERT INTO esgf_dashboard.country VALUES ('247', 'EU', 'No country', 'EU');
 INSERT INTO esgf_dashboard.country VALUES ('248', 'AP', 'No country', 'AS');
+
+SET search_path = public, pg_catalog;
